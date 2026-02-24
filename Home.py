6741f642534689f13b7f75c90d25ee8b93685c44
ginraylum-blip <ginraylum@gmail.com>
2026-02-24 23:07:01 +0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Lum-Ashton | Black-Scholes Option Pricer",
+    page_icon="📈",
+    layout="wide"
+)
+
+st.title("Directory")
+st.write("This is the project directory page. Navigate to other pages using the sidebar.")
+
+
+st.write("done by Ashton Lum https://www.linkedin.com/in/lum-ashton/")
